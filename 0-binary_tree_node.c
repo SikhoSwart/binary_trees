7b45cb3d7f4b941bf680a25319e2abe627f7f2a4
SikhoSwart <sikhoswart@gmail.com>
2023-10-04 11:07:00 -0700

    Task 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - Create a biinary tree node
+ * @parent: pointer to node parent
+ * @value: Value of the node
+ * Return: a pointer to the new node, or NULL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	return (new);
+}
